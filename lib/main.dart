53dc4185d65a8ec3d53d20fd7e90651f5e6ebf1f
shajeehsanal <shajeehsanal@gmail.com>
2024-04-13 14:19:50 +0530

    connected to firebase

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,12 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:todo_app/features/home/home.dart';
 import 'package:todo_app/global/global_providers.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const ProviderScope(child: MyApp()));
 }
 
@@ -21,19 +25,23 @@ class _MyAppState extends ConsumerState<MyApp> {
   @override
   void initState() {
     super.initState();
-    WidgetsBinding.instance.addPostFrameCallback((timeStamp) async {
-      preferences = await SharedPreferences.getInstance();
-      final currentTheme = preferences.getString('themeMode') ?? '';
-      ref.read(themeModeProvider.notifier).update((state) {
-        ThemeMode themeMode = state;
-        if (currentTheme == 'Light') {
-          themeMode = ThemeMode.light;
-        } else if (currentTheme == 'Dark') {
-          themeMode = ThemeMode.dark;
-        }
-        return themeMode;
-      });
-    });
+    WidgetsBinding.instance.addPostFrameCallback(
+      (timeStamp) async {
+        preferences = await SharedPreferences.getInstance();
+        final currentTheme = preferences.getString('themeMode') ?? '';
+        ref.read(themeModeProvider.notifier).update(
+          (state) {
+            ThemeMode themeMode = state;
+            if (currentTheme == 'Light') {
+              themeMode = ThemeMode.light;
+            } else if (currentTheme == 'Dark') {
+              themeMode = ThemeMode.dark;
+            }
+            return themeMode;
+          },
+        );
+      },
+    );
   }
 
   @override
