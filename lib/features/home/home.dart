36665ceee98eac1cd2f9791f2ca701f1d8179a9d
shajeehsanal <shajeehsanal@gmail.com>
2024-04-13 13:24:16 +0530

    Added option to change theme mode

--- lib/features/home/home.dart
+++ lib/features/home/home.dart
@@ -1,5 +1,8 @@
+import 'package:dropdown_button2/dropdown_button2.dart';
 import 'package:flutter/material.dart';
-import 'package:todo_app/features/settings/settings_page.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:todo_app/global/global_providers.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -9,35 +12,30 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  List<DropdownMenuItem> dropdownItems = [];
+  late SharedPreferences preferences;
+
+  @override
+  void initState() {
+    super.initState();
+    dropdownItems = [
+      const DropdownMenuItem(value: 'System', child: Icon(Icons.computer)),
+      const DropdownMenuItem(value: 'Light', child: Icon(Icons.light_mode)),
+      const DropdownMenuItem(value: 'Dark', child: Icon(Icons.dark_mode)),
+    ];
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: Drawer(
+      drawer: const Drawer(
         child: Column(
           children: [
-            const UserAccountsDrawerHeader(
+            UserAccountsDrawerHeader(
               accountName: Text('User Name'),
               accountEmail: Text('User Email'),
               currentAccountPicture: CircleAvatar(
                 backgroundColor: Colors.blueGrey,
-              ),
-            ),
-            Expanded(
-              child: Column(
-                children: [
-                  ListTile(
-                    leading: const Icon(Icons.settings),
-                    title: const Text('Settings'),
-                    trailing: const Icon(Icons.navigate_next),
-                    onTap: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => const SettingsPage(),
-                          ));
-                    },
-                  ),
-                ],
               ),
             ),
           ],
@@ -52,6 +50,48 @@ class _HomeScreenState extends State<HomeScreen> {
         ),
         centerTitle: true,
         iconTheme: const IconThemeData(color: Colors.white),
+        actions: [
+          Consumer(
+            builder: (context, ref, child) {
+              final themeMode = ref.watch(themeModeProvider);
+              String theme = themeMode == ThemeMode.system
+                  ? 'System'
+                  : themeMode == ThemeMode.light
+                      ? 'Light'
+                      : 'Dark';
+
+              return Padding(
+                padding: const EdgeInsets.only(right: 5),
+                child: DropdownButtonHideUnderline(
+                  child: DropdownButton2(
+                    items: dropdownItems,
+                    value: theme,
+                    onChanged: (value) async {
+                      preferences = await SharedPreferences.getInstance();
+                      preferences.setString(
+                        'themeMode',
+                        value.toString().trim(),
+                      );
+                      if (value == 'System') {
+                        ref
+                            .read(themeModeProvider.notifier)
+                            .update((state) => ThemeMode.system);
+                      } else if (value == 'Light') {
+                        ref
+                            .read(themeModeProvider.notifier)
+                            .update((state) => ThemeMode.light);
+                      } else {
+                        ref
+                            .read(themeModeProvider.notifier)
+                            .update((state) => ThemeMode.dark);
+                      }
+                    },
+                  ),
+                ),
+              );
+            },
+          )
+        ],
       ),
       body: Container(),
       floatingActionButton: FloatingActionButton(
--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:todo_app/features/home/home.dart';
-import 'package:todo_app/global/global_variables.dart';
+import 'package:todo_app/global/global_providers.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const ProviderScope(child: MyApp()));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends ConsumerStatefulWidget {
   const MyApp({super.key});
 
   @override
+  ConsumerState<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends ConsumerState<MyApp> {
+  late SharedPreferences preferences;
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((timeStamp) async {
+      preferences = await SharedPreferences.getInstance();
+      final currentTheme = preferences.getString('themeMode') ?? '';
+      ref.read(themeModeProvider.notifier).update((state) {
+        ThemeMode themeMode = state;
+        if (currentTheme == 'Light') {
+          themeMode = ThemeMode.light;
+        } else if (currentTheme == 'Dark') {
+          themeMode = ThemeMode.dark;
+        }
+        return themeMode;
+      });
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final themeMode = ref.watch(themeModeProvider);
+
     return MaterialApp(
       theme: ThemeData(
         colorScheme: ColorScheme.light(primary: Colors.blue.shade900),
