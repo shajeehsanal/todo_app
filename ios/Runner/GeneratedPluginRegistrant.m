eda95b42322845126de3a8626417d989933cf57b
shajeehsanal <shajeehsanal@gmail.com>
2024-05-12 17:56:54 +0530

    ui changed

---
+++ ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,28 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<firebase_core/FLTFirebaseCorePlugin.h>)
+#import <firebase_core/FLTFirebaseCorePlugin.h>
+#else
+@import firebase_core;
+#endif
+
+#if __has_include(<shared_preferences_foundation/SharedPreferencesPlugin.h>)
+#import <shared_preferences_foundation/SharedPreferencesPlugin.h>
+#else
+@import shared_preferences_foundation;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [FLTFirebaseCorePlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTFirebaseCorePlugin"]];
+  [SharedPreferencesPlugin registerWithRegistrar:[registry registrarForPlugin:@"SharedPreferencesPlugin"]];
+}
+
+@end
--- lib/features/home/home.dart
+++ lib/features/home/home.dart
@@ -4,16 +4,17 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:todo_app/global/global_providers.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-class HomeScreen extends StatefulWidget {
+class HomeScreen extends ConsumerStatefulWidget {
   const HomeScreen({super.key});
 
   @override
-  State<HomeScreen> createState() => _HomeScreenState();
+  ConsumerState<HomeScreen> createState() => _HomeScreenState();
 }
 
-class _HomeScreenState extends State<HomeScreen> {
+class _HomeScreenState extends ConsumerState<HomeScreen> {
   List<DropdownMenuItem> dropdownItems = [];
   late SharedPreferences preferences;
+  final selectedIndexProvider = StateProvider<int>((ref) => 0);
 
   @override
   void initState() {
@@ -27,6 +28,7 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
+    final selectedIndex = ref.watch(selectedIndexProvider);
     return Scaffold(
       drawer: const Drawer(
         child: Column(
@@ -41,58 +43,95 @@ class _HomeScreenState extends State<HomeScreen> {
           ],
         ),
       ),
-      appBar: AppBar(
-        title: const Text('Todo List'),
-        elevation: 10,
-        centerTitle: true,
-        iconTheme: const IconThemeData(color: Colors.white),
-        actions: [
-          Consumer(
-            builder: (context, ref, child) {
-              final themeMode = ref.watch(themeModeProvider);
-              String theme = themeMode == ThemeMode.system
-                  ? 'System'
-                  : themeMode == ThemeMode.light
-                      ? 'Light'
-                      : 'Dark';
-
-              return Padding(
-                padding: const EdgeInsets.only(right: 5),
-                child: DropdownButtonHideUnderline(
-                  child: DropdownButton2(
-                    items: dropdownItems,
-                    value: theme,
-                    onChanged: (value) async {
-                      preferences = await SharedPreferences.getInstance();
-                      preferences.setString(
-                        'themeMode',
-                        value.toString().trim(),
-                      );
-                      if (value == 'System') {
-                        ref
-                            .read(themeModeProvider.notifier)
-                            .update((state) => ThemeMode.system);
-                      } else if (value == 'Light') {
-                        ref
-                            .read(themeModeProvider.notifier)
-                            .update((state) => ThemeMode.light);
-                      } else {
-                        ref
-                            .read(themeModeProvider.notifier)
-                            .update((state) => ThemeMode.dark);
-                      }
-                    },
-                  ),
+      bottomNavigationBar: BottomAppBar(
+        color: Colors.blue.shade900,
+        shape: const CircularNotchedRectangle(),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            IconButton(
+                icon: const Icon(
+                  Icons.abc,
+                  color: Colors.white,
                 ),
-              );
-            },
-          )
-        ],
+                onPressed: () {}),
+            IconButton(
+                icon: const Icon(
+                  Icons.abc,
+                  color: Colors.white,
+                ),
+                onPressed: () {}),
+            IconButton(
+                icon: const Icon(
+                  Icons.abc,
+                  color: Colors.white,
+                ),
+                onPressed: () {}),
+            IconButton(
+                icon: const Icon(
+                  Icons.abc,
+                  color: Colors.white,
+                ),
+                onPressed: () {}),
+          ],
+        ),
       ),
+      // appBar: AppBar(
+      //   title: const Text('Todo List'),
+      //   elevation: 10,
+      //   centerTitle: true,
+      //   iconTheme: const IconThemeData(color: Colors.white),
+      //   actions: [
+      //     Consumer(
+      //       builder: (context, ref, child) {
+      //         final themeMode = ref.watch(themeModeProvider);
+      //         String theme = themeMode == ThemeMode.system
+      //             ? 'System'
+      //             : themeMode == ThemeMode.light
+      //                 ? 'Light'
+      //                 : 'Dark';
+
+      //         return Padding(
+      //           padding: const EdgeInsets.only(right: 5),
+      //           child: DropdownButtonHideUnderline(
+      //             child: DropdownButton2(
+      //               items: dropdownItems,
+      //               value: theme,
+      //               onChanged: (value) async {
+      //                 preferences = await SharedPreferences.getInstance();
+      //                 preferences.setString(
+      //                   'themeMode',
+      //                   value.toString().trim(),
+      //                 );
+      //                 if (value == 'System') {
+      //                   ref
+      //                       .read(themeModeProvider.notifier)
+      //                       .update((state) => ThemeMode.system);
+      //                 } else if (value == 'Light') {
+      //                   ref
+      //                       .read(themeModeProvider.notifier)
+      //                       .update((state) => ThemeMode.light);
+      //                 } else {
+      //                   ref
+      //                       .read(themeModeProvider.notifier)
+      //                       .update((state) => ThemeMode.dark);
+      //                 }
+      //               },
+      //             ),
+      //           ),
+      //         );
+      //       },
+      //     )
+      //   ],
+      // ),
       body: Container(),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: const Icon(Icons.add),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: FloatingActionButton(
+          onPressed: () {},
+          child: const Icon(Icons.add),
+        ),
       ),
     );
   }
